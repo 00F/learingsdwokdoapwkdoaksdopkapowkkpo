e66a8c60fbfd1372222c30ef4cfa6544d121fd15
` 0ff <39999036+00F@users.noreply.github.com>
2018-10-15 14:36:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const developers = ['320423357709549568'];
 client.on('message', message => {
         if (!developers.includes(message.author.id)) return;
   if (message.content === '712') {
-  let channel = client.channels.get('501226265701908480');
+  let channel = client.channels.get('458667741222797332');
 
   channel.join()
   .then(connection => console.log('Connected'))
