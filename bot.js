7b2c8541f0920d5239b8fb45ecb1a540344df7c8
` 0ff <39999036+00F@users.noreply.github.com>
2018-10-15 06:58:58 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const developers = ['320423357709549568'];
+client.on('message', message => {
+        if (!developers.includes(message.author.id)) return;
+  if (message.content === '712') {
+  let channel = client.channels.get('501226265701908480');
+
+  channel.join()
+  .then(connection => console.log('Connected'))
+  .catch(console.error);
+    
+  }
+          
+});
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
